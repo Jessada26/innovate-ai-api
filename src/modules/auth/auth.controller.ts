3af8e6f916fc75ddfb051a26ef50b3de97e23d4c
Jessadapong Rattana <jessadapong_rat@truecorp.co.th>
2025-07-09 20:39:59 +0700

    [BE]: open set cookie for front end at auth login logout

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -15,18 +15,18 @@ export class AuthController {
   @Post('auth/login')
   async login(@Request() req, @Res() res: Response) {
     const token = await this.authService.login(req.user);
-    //res.cookie('access_token', token.access_token); //, { httpOnly: true, secure: true }
+    res.cookie('access_token', token.access_token); //, { httpOnly: true, secure: true }
     return res.status(200).send(token);
   }
 
-  // @UseGuards(JwtAuthGuard)
-  // @Post('auth/logout')
-  // async logout(@Res() res: Response) {
-  //   // Clear the 'access_token' cookie
-  //   res.clearCookie('access_token');
-  //   // Send a response if needed
-  //   return res.status(200).send({ message: 'Logout successful' });
-  // }
+  @UseGuards(JwtAuthGuard)
+  @Post('auth/logout')
+  async logout(@Res() res: Response) {
+    // Clear the 'access_token' cookie
+    res.clearCookie('access_token');
+    // Send a response if needed
+    return res.status(200).send({ message: 'Logout successful' });
+  }
 
   @Roles(ROLE.GUEST, ROLE.MAID_SUPERVISOR)
   @UseGuards(JwtAuthGuard, RolesGuard)
